b508e35b5380090180fd655d23b5d5ee6d271c15
AntoniaCarlstrom <112555651+AntoniaCarlstrom@users.noreply.github.com>
2023-05-08 12:51:12 +0200

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "../css/App.css";
+import Nav from "./Nav";
+import Cart from "./Cart";
+import Shop from "./Shop";
+import {ShopContextProvider} from "../context/ShopContext";
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+      <footer className="footer">
+        <div>
+          <p>987 Main Street</p>
+          <p>Texas, USA 548</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
